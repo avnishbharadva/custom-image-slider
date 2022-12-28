7ddc1a402d8f4e4578cb5727a20bc7262691a3b8
Avnish Bharadva <avnishbharadva@gmail.com>
2022-12-28 15:02:34 +0530

    Custom Image Slider

--- index.js
+++ index.js
@@ -2,33 +2,6 @@ let img_gal = document.getElementById("img_gal");
 
 let img_arr = ["./images/img1.jpg","./images/img2.jpg","./images/img3.jpg","./images/img4.jpg","./images/img5.jpg","./images/img6.jpg"]
 
-// <!-- Modal -->
-// let modalWrap = null;
-
-// const showModal = (index) => {
-//     modalWrap = document.createElement('div');
-//     modalWrap.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//     <div class="modal-dialog">
-//       <div class="modal-content">
-//         <div class="modal-header">
-//           <h1 class="modal-title fs-5" id="exampleModalLabel">${index}</h1>
-//           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//         </div>
-//         <div class="modal-body">
-//           ...
-//         </div>
-//         <div class="modal-footer">
-//           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//           <button type="button" class="btn btn-primary">Save changes</button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>`;
-
-//   document.body.append(modalWrap);
-
-//   var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-// }
 const pre_pic = () => {
     let il = img_arr.length;
     let modalTitle = document.getElementById("exampleModalLabel");
